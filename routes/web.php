1860e2cee93eaf66035f4d5739992f8b33f36f9b
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-11-19 10:02:08 -0800

    todooo

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,17 @@ Route::get('/', function () {
 });
 
 Route::get('/saludar/{name}/{lastName?}', function ($name = "popo", $lastName = "Doc") {
-    return "hello world $name $lastName";
+    return "hello $name $lastName";
+});
+
+Route::get('/hello/{name?}', function ($name = "popo") {
+    return "wep $name";
 });
 
 Route::get('/operation/{type}/{a}/{b}', function ($type,$a = 1, $b=1) {
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return response()->json(['error' => 'NUMEROS PADRINO'], 400);
+    }
     switch ($type) {
         case 'sumar':
             return $a + $b;
